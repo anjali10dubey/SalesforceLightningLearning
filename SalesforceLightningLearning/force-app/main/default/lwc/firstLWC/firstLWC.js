d200cf99ebe2f1ef7f41053d372bdea73abda2b6
Anjali Dubey <anjalidubey@Anjalis-Air.lan>
2023-01-06 23:26:28 -0500

    First Commit

---
+++ SalesforceLightningLearning/force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class MyFirstLWC extends LightningElement {
+
+    name = '';
+
+    updateName(event) {
+        this.name = event.target.value;
+    }
+}
+
