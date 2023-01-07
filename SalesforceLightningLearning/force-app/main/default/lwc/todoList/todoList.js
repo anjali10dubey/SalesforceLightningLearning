f22f1acad595166289ca283c52013071297b69fa
anjali10dubey <anjali10dubey@gmail.com>
2023-01-07 18:06:57 -0500

    Fifth Commit

--- SalesforceLightningLearning/force-app/main/default/lwc/todoList/todoList.js
+++ SalesforceLightningLearning/force-app/main/default/lwc/todoList/todoList.js
@@ -1,8 +1,11 @@
 import { LightningElement } from 'lwc';
 
 export default class TodoList extends LightningElement {
+    /*
+    *   This method is used to update new task variable
+    *   with the value specified in the input field
+    */
     newTask='';
-    
     updateTask(event){
         this.newTask=event.target.value;
     }
