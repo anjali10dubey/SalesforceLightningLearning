20566cfe8e96e5da1f84329653ec77a8c9070fad
anjali10dubey <anjali10dubey@gmail.com>
2023-01-08 01:06:00 -0500

    Seventh Commit

--- SalesforceLightningLearning/force-app/main/default/lwc/todoList/todoList.js
+++ SalesforceLightningLearning/force-app/main/default/lwc/todoList/todoList.js
@@ -1,25 +1,25 @@
 import { LightningElement, track } from 'lwc';
 
 export default class TodoList extends LightningElement {
-    /*
-      This method is used to render list item.
-    */
+/*
+    This method is used to render list item.
+*/
    //track is a decorator. It helps in instant rendering. (No longer required for single variable, but it must be used for object or multi valued eg- array)
    @track
     TodoList=[];
 
-    /*
+/*
       This method is used to update new task variable
       with the value specified in the input field
-    */
+*/
     newTask='';
     updateTask(event){
         this.newTask=event.target.value;
     }
 
-    /*
+/*
       This method is used to add new task.
-    */
+*/
     addTask(event){
         //Push function is used to add element at the end of the array.
         //Unshift funcion is used to element at the start of the array. e.g., twitter post
@@ -30,6 +30,40 @@ export default class TodoList extends LightningElement {
             }
         );
         this.newTask='';
-        console.log(this.TodoList);
+        // console.log(this.TodoList); 
     }
+    deleteTask(event){
+        //console.log(event.target.name);
+        
+/*
+      It's brute force method to find index.
+      --------------------------------------
+        let toDeleteIndex;
+        for(let i=0;i<this.TodoList.length;i++)
+        {
+            if(this.TodoList[i].id===event.target.name)
+            {
+                toDeleteIndex = i;
+            }
+        }
+        this.TodoList.splice(toDeleteIndex, 1);
+*/
+
+/*
+       This is the already present JavaScript method to find index number.
+       ------------------------------------------------------------------
+        this.TodoList.splice(this.TodoList.findIndex(
+            function(TodoList){
+                return TodoList.id === event.target.name;
+            }
+        ), 1);
+ */ 
+
+/*      More Optimization On Delete (Single Line)
+        -----------------------------------------
+*/      
+        this.TodoList.splice(this.TodoList.findIndex(TodoList => TodoList.id === event.target.name),1);
+            
+    }
+
 }
